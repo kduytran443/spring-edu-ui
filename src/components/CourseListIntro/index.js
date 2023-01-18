375942e4f066abb3426705500d36cf481066e662
kduytran443 <trankhanhduy18@gmail.com>
2023-01-18 14:57:09 +0700

    Category page

--- src/components/CourseListIntro/index.js
+++ src/components/CourseListIntro/index.js
@@ -1,45 +1,75 @@
 import { faDiagramNext, faForward, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Divider } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { CATEGORY_PAGE_URL } from '~/constants';
 import CourseItemIntro from '../CourseItemIntro';
 
-function CourseListIntro({ listItem = [1, 1, 1, 1, 1, 1, 1, 1], title = '', icon = '', link = '' }) {
+function CourseListIntro({
+    listItem = [1, 1, 1, 1, 1, 1, 1, 1],
+    title = '',
+    icon = '',
+    link = '',
+    hiddenHeader = false,
+}) {
     //item: image, title, description, avatar, userLink, courseLink, review, courseQuantity, price, time
 
+    console.log(hiddenHeader);
+
+    const navigate = useNavigate();
+
+    const navigateToPage = () => {
+        navigate(CATEGORY_PAGE_URL);
+    };
+
     return (
-        <div className="w-full">
-            <div className="px-2 flex-col flex">
-                <div className="flex flex-row items-center justify-start mb-4 mt-8 flex-wrap">
-                    <div className="flex flex-row items-center flex-wrap">
-                        <div className="md:w-[40px] w-[36px] mr-4 select-none overflow-hidden rounded">
-                            <img className="w-full" alt="logo" src={icon} />
+        <div className="w-full flex flex-col items-center">
+            {!hiddenHeader && (
+                <div className="pb-2 flex-col flex w-full">
+                    <div className="flex flex-row items-center justify-between mb-4 mt-8 flex-wrap">
+                        <div className="flex flex-row items-center flex-wrap">
+                            <div className="md:w-[40px] w-[36px] mr-4 select-none overflow-hidden rounded">
+                                <img className="w-full" alt="logo" src={icon} />
+                            </div>
+                            <span className="text-2xl md:text-3xl font-black">{title}</span>
                         </div>
-                        <span className="text-2xl md:text-3xl font-black">{title}</span>
+                        <div className="ml-8">
+                            <Button
+                                onClick={navigateToPage}
+                                variant="text"
+                                startIcon={<FontAwesomeIcon icon={faForward} />}
+                            >
+                                Xem thêm
+                            </Button>
+                        </div>
                     </div>
-                    <div className="ml-8">
-                        <Button variant="text" startIcon={<FontAwesomeIcon icon={faForward} />}>
-                            Xem thêm
-                        </Button>
+                    <div className="ml-4">
+                        <FontAwesomeIcon icon={faUser} className="mr-2" />
+                        <span>
+                            <b>276.368+</b> người khác đã học
+                        </span>
                     </div>
+                    <div></div>
                 </div>
-                <div className="ml-4">
-                    <FontAwesomeIcon icon={faUser} className="mr-2" />
-                    <span>
-                        <b>276.368+</b> người khác đã học
-                    </span>
-                </div>
-            </div>
+            )}
             <div className="overflow-x-scroll w-full">
-                <ul className="flex flex-row items-center ">
+                <ul className="flex flex-row items-center md:flex-wrap">
                     {listItem.map((item, index) => {
                         return (
-                            <li className="min-w-[345px] mx-2 my-4">
+                            <li className="min-w-[328px] max-w-[328px] mx-2 ">
                                 <CourseItemIntro />
                             </li>
                         );
                     })}
                 </ul>
             </div>
+            {!hiddenHeader && (
+                <div>
+                    <Button onClick={navigateToPage} variant="text" startIcon={<FontAwesomeIcon icon={faForward} />}>
+                        Xem thêm
+                    </Button>
+                </div>
+            )}
         </div>
     );
 }
